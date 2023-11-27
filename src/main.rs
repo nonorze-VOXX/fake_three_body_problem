af81e6e022d10e58690ea618a9d7b6e8f29fe6fe
nonorze-VOXX <appieosugood@gmail.com>
2023-11-27 13:54:09 +0800

    look white background

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use bevy::prelude::*;
+
+const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
+// const BOX_OLOR: Color = Color::rgb(0.3, 0.3, 0.7);
 fn main() {
     println!("Hello, world!");
+
+    let _run = App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(BACKGROUND_COLOR))
+        .add_systems(Startup, setup)
+        .add_systems(Update, bevy::window::close_on_esc)
+        .run();
+}
+
+#[derive(Component)]
+struct GroupBox;
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+    // commands.spawn();
 }
