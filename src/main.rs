35d46e103e4d77ec1cbbb6ba8037f19030cfa4cf
nonorze-VOXX <appieosugood@gmail.com>
2024-01-14 14:15:38 +0800

    LGF but better

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,56 @@
-use bevy::{prelude::*, render::render_resource::resource_macros, window::close_on_esc};
-use bevy_egui::{egui, EguiContexts, EguiPlugin};
+use bevy::{
+    core_pipeline::core_2d::graph::input,
+    ecs::query,
+    prelude::*,
+    render::{mesh, render_resource::resource_macros},
+    window::close_on_esc,
+};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(EguiPlugin)
         // Systems that create Egui widgets should be run during the `CoreSet::Update` set,
         // or after the `EguiSet::BeginFrame` system (which belongs to the `CoreSet::PreUpdate` set).
-        .add_systems(Startup, add_Task)
-        .add_systems(Update, show_task)
+        .add_systems(Startup, create_world)
+        .add_systems(Startup, create_a_card)
         .add_systems(Update, close_on_esc)
+        .add_systems(Update, move_card)
         .run();
 }
-
-fn show_task(mut contexts: EguiContexts, query: Query<&Task>) {
-    for task in &query {
-        egui::Window::new(task.0.to_string()).show(contexts.ctx_mut(), |ui| {
-            ui.label(task.0.to_string());
-        });
+fn create_world(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+fn create_a_card(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.25, 0.25, 0.75),
+                custom_size: Some(Vec2::new(50.0, 100.0)),
+                ..default()
+            },
+            transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
+            ..default()
+        },
+        Card {
+            name: "test".to_string(),
+        },
+    ));
+}
+fn move_card(
+    time: Res<Time>,
+    mut query: Query<(&mut Transform, &Card)>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    for (mut transform, card) in query.iter_mut() {
+        transform.translation.x += time.delta_seconds() * 100.0;
+        println!("Card name: {}", card.name);
     }
 }
 #[derive(Component)]
-struct Task(String);
-fn add_Task(mut commands: Commands) {
-    commands.spawn((Task("Elaina Proctor".to_string())));
-    commands.spawn((Task("Renzo Hume".to_string())));
-    commands.spawn((Task("Zayna Nieves".to_string())));
+struct Card {
+    name: String,
 }
