1097e689e15f32da3f4321027374bed31f058574
nonorze-VOXX <appieosugood@gmail.com>
2024-01-14 17:23:39 +0800

    let it like golang

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use bevy::{transform, window::PrimaryWindow};
 use std::{borrow::Borrow, fs::copy};
 
 use bevy::{
@@ -17,16 +18,33 @@ fn main() {
         // Systems that create Egui widgets should be run during the `CoreSet::Update` set,
         // or after the `EguiSet::BeginFrame` system (which belongs to the `CoreSet::PreUpdate` set).
         .add_systems(Startup, create_world)
+        .add_systems(Startup, create_a_button)
         .add_systems(Startup, create_a_card)
         .add_systems(Update, close_on_esc)
         .add_systems(Update, move_card)
-        .add_systems(Update, cursor_position)
+        .add_systems(Update, click_kanban_button)
         .add_systems(Update, handle_click_card)
         .run();
 }
+fn click_kanban_button(mut commands: Commands) {}
 fn create_world(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
+fn create_a_button(mut commands: Commands) {
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.25, 0.25, 0.75),
+                custom_size: Some(Vec2::new(50.0, 50.0)),
+                ..default()
+            },
+            transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
+            ..default()
+        },
+        KanbanButton { ..default() },
+    ));
+}
+
 fn create_a_card(mut commands: Commands) {
     commands.spawn((
         SpriteBundle {
@@ -90,16 +108,33 @@ fn move_card(
     buttons: Res<Input<MouseButton>>,
     q_windows: Query<&Window, With<PrimaryWindow>>,
 ) {
-    if buttons.pressed(MouseButton::Left) {
-        for (mut transform, sprite, card) in query.iter_mut() {
-            if card.clicked {
-                if let Some(position) = q_windows.single().cursor_position() {
-                    let mouse_pos = mouse_to_world(position, &q_windows);
-                    transform.translation = mouse_pos;
-                    break;
-                }
-            }
-        }
+    if !buttons.pressed(MouseButton::Left) {
+        return;
+    }
+
+    if q_windows.single().cursor_position().is_none() {
+        return;
+    }
+
+    let position = q_windows.single().cursor_position().unwrap();
+
+    let clicked_card = query.iter_mut().find(|(_, _, card)| card.clicked);
+    if clicked_card.is_none() {
+        return;
+    }
+
+    let (mut transform, _, _) = clicked_card.unwrap();
+
+    let mouse_pos = mouse_to_world(position, &q_windows);
+    transform.translation = mouse_pos;
+}
+#[derive(Component)]
+struct KanbanButton {
+    clicked: bool,
+}
+impl Default for KanbanButton {
+    fn default() -> Self {
+        KanbanButton { clicked: false }
     }
 }
 #[derive(Component)]
@@ -113,16 +148,5 @@ impl Default for Card {
             name: "default".to_string(),
             clicked: false,
         }
-    }
-}
-
-use bevy::window::PrimaryWindow;
-
-fn cursor_position(q_windows: Query<&Window, With<PrimaryWindow>>) {
-    // Games typically only have one window (the primary window)
-    if let Some(position) = q_windows.single().cursor_position() {
-        //println!("Cursor is inside the primary window, at {:?}", position);
-    } else {
-        //println!("Cursor is not in the game window.");
     }
 }
