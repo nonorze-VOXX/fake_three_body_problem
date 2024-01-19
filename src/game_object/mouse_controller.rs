2923d4ba378b1c712b78940de6131fe6b72c7ec8
nonorze-VOXX <appieosugood@gmail.com>
2024-01-19 16:23:57 +0800

    upside detect down side detect

--- src/game_object/mouse_controller.rs
+++ src/game_object/mouse_controller.rs
@@ -10,15 +10,19 @@ pub struct MousePlugin;
 
 impl Plugin for MousePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, update_hover)
+        app.add_event::<MouseClickEvent>()
+            .add_systems(Update, update_hover)
             .add_systems(Update, update_click)
+            .add_systems(Update, update_touch)
             .add_systems(Update, debug_click);
     }
 }
 
+#[derive(Event)]
+struct MouseClickEvent(Entity);
 #[derive(Component)]
 pub struct MouseComponent {
-    clicked: bool,
+    touched: bool,
     hover: bool,
     upside_triggered: bool,
     downside_triggered: bool,
@@ -26,7 +30,7 @@ pub struct MouseComponent {
 impl Default for MouseComponent {
     fn default() -> Self {
         Self {
-            clicked: false,
+            touched: false,
             hover: false,
             upside_triggered: false,
             downside_triggered: false,
@@ -38,10 +42,12 @@ fn debug_click(mut query: Query<(&mut Sprite, &mut MouseComponent)>) {
     query.iter_mut().for_each(|(mut sprite, ouseComponent)| {
         //println!("find sprite and mouse component");
         if (ouseComponent.upside_triggered) {
+            println!("upside triggered");
             sprite.color = Color::RED;
         } else if (ouseComponent.downside_triggered) {
+            println!("downside triggered");
             sprite.color = Color::BLUE;
-        } else if (ouseComponent.clicked) {
+        } else if (ouseComponent.touched) {
             sprite.color = Color::GREEN;
         } else {
             sprite.color = Color::WHITE;
@@ -49,29 +55,26 @@ fn debug_click(mut query: Query<(&mut Sprite, &mut MouseComponent)>) {
     })
 }
 fn update_click(
+    mut ev_click: EventWriter<MouseClickEvent>,
     buttons: Res<Input<MouseButton>>,
-    mut query: Query<(&Transform, &Sprite, &mut MouseComponent)>,
+    mut query: Query<(Entity, &mut MouseComponent)>,
 ) {
-    if buttons.just_pressed(MouseButton::Left) {
-        query.iter_mut().for_each(|(_, _, mut mouse_component)| {
-            mouse_component.clicked = mouse_component.hover;
-            mouse_component.upside_triggered = mouse_component.hover;
-        });
-    } else {
-        query.iter_mut().for_each(|(_, _, mut mouse_component)| {
-            mouse_component.upside_triggered = false;
-        });
-    }
-    if buttons.just_released(MouseButton::Left) {
-        query.iter_mut().for_each(|(_, _, mut mouse_component)| {
-            mouse_component.downside_triggered = mouse_component.hover && mouse_component.clicked;
-            mouse_component.clicked = false;
-        });
-    } else {
-        query.iter_mut().for_each(|(_, _, mut mouse_component)| {
-            mouse_component.downside_triggered = false;
-        });
-    }
+    query.iter_mut().for_each(|(entity, mut mouse_component)| {
+        let upside = buttons.just_pressed(MouseButton::Left) && mouse_component.hover;
+        mouse_component.upside_triggered = upside;
+    });
+    query.iter_mut().for_each(|(entity, mut mouse_component)| {
+        mouse_component.downside_triggered =
+            buttons.just_released(MouseButton::Left) && mouse_component.hover;
+        if (mouse_component.downside_triggered) {
+            ev_click.send(MouseClickEvent(entity));
+        }
+    });
+}
+fn update_touch(buttons: Res<Input<MouseButton>>, mut query: Query<(&mut MouseComponent)>) {
+    query.iter_mut().for_each(|(mut mouse_component)| {
+        mouse_component.touched = buttons.pressed(MouseButton::Left) && mouse_component.hover;
+    });
 }
 
 fn update_hover(
